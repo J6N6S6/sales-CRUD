b29b05a48aa9fdf8bec548f4ed3088e015fa0e3d
J6N6S6 <jonasscandeias@gmail.com>
2024-07-24 20:20:33 -0300

    Primeiro commit com conexão do BD

---
+++ sales CRUD/Context/SalesCRUDDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sales_CRUD.Context
+{
+    public class SalesCRUDDbContext : DbContext
+    {
+        public SalesCRUDDbContext(DbContextOptions<SalesCRUDDbContext> options) : base (options) { }
+    }
+}
